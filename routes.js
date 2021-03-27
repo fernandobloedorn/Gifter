fd84b304a12470737683d9e9f64b65164edb28d3
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-03-27 10:30:48 -0300

    Add products

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const routes = express.Router();
-const ProductController = require("./controller/product-controller");
+const ProductController = require("./controller/ProductController");
 
 routes.get("/", function(req, res) {
   return res.send("Minha primeira rota!");
